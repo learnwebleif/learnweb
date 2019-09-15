513083c037cff1c6cf6a4002c9294a42a9d04818
Leif Johansson <leif.lernia@gmail.com>
2019-09-15 14:49:44 +0200

    deleted unused css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,20 @@
 <style>
-	h1, figure, p {
+	/* h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	} */
+
+	figure {
 		text-align: center;
 		margin: 0 auto;
 	}
 
-	h1 {
+	/* h1 {
 		font-size: 2.8em;
 		text-transform: uppercase;
 		font-weight: 700;
 		margin: 0 0 0.5em 0;
-	}
+	} */
 
 	figure {
 		margin: 0 0 1em 0;
@@ -21,14 +26,14 @@
 		margin: 0 0 1em 0;
 	}
 
-	p {
+	/* p {
 		margin: 1em auto;
-	}
+	} */
 
 	@media (min-width: 480px) {
-		h1 {
+		/* h1 {
 			font-size: 4em;
-		}
+		} */
 	}
 </style>
 
