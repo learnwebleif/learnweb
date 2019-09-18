007c87bdb6e7b229d135bba106afa069005cb724
Leif Johansson <leif.lernia@gmail.com>
2019-09-18 11:26:29 +0200

    Updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,47 +1,88 @@
 <style>
-	/* h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	} */
+  @import url("https://fonts.googleapis.com/css?family=Overpass|Ruluko&display=swap");
 
-	figure {
-		text-align: center;
-		margin: 0 auto;
-	}
+  #logo {
+    margin-left: 10px;
+    margin-top: 40px;
+  }
 
-	/* h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	} */
+  h3 {
+    margin-left: 12px;
+    font-family: "Overpass", sans-serif;
+    font-size: 1.2rem;
+    font-weight: 300;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  #small {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  .startImage-small {
+    display: block
+  }
 
-	/* p {
-		margin: 1em auto;
-	} */
+  .startImage-big {
+    visibility: hidden;
+  }
 
-	@media (min-width: 480px) {
-		/* h1 {
-			font-size: 4em;
-		} */
-	}
+  @media only screen and (min-width: 415px) {
+    .page-container {
+      background: url("");
+      background-size: cover;
+    }
+    .startImage-small {
+      display: none;
+      z-index: -1;
+    }
+    .startImage-big {
+    visibility: visible;
+  }
+    img {
+    width: 100%;
+    /* max-width: 400px; */
+    margin: 0 0 1em 0;
+  }
+  }
 </style>
 
 <svelte:head>
-	<title>learnweb</title>
+  <title>learnweb</title>
 </svelte:head>
 
-<figure>
-	<img alt='logo' src='great-success.png'>
-</figure>
-
+<div class="page-container">
+  <svg
+    id="logo"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    style="isolation:isolate"
+    viewBox="230.5 136.75 183 55.47"
+    width="169pt"
+    height="55.47pt">
+    <defs>
+      <clipPath id="_clipPath_qAVaLgp9g35h5DwJHxwUtjy0breOmNEL">
+        <rect x="230.5" y="136.75" width="183" height="55.47" />
+      </clipPath>
+    </defs>
+    <g clip-path="url(#_clipPath_qAVaLgp9g35h5DwJHxwUtjy0breOmNEL)">
+      <text
+        transform="matrix(1,0,0,1,230.5,180.622)"
+        style="font-family:'Ruluko';font-weight:400;font-size:48px;font-style:normal;fill:#4a4a55;stroke:none;">
+        learnweb
+      </text>
+      <text
+        transform="matrix(1,0,0,1,230.5,180.652)"
+        style="font-family:'Ruluko';font-weight:400;font-size:48px;font-style:normal;fill:#4a4a55;stroke:none;">
+        learnweb
+      </text>
+    </g>
+  </svg>
+  <h3>Uveckling av digitala lärresurser</h3>
+  <div class="startImage-small">
+    <img id="small" style="padding-top:3rem;" src="larresurs.png" alt="Lärresurs" />
+  </div>
+  <div class="startImage-big">
+    <img width="800px" style="padding-top:3rem;" src="larresurs_big.jpg" alt="Lärresurs" />
+  </div>
+</div>
