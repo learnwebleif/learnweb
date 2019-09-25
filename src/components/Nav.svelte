891fb31630d86540593fde1c634a9c7f90d7fb88
Leif Johansson <leif.lernia@gmail.com>
2019-09-25 16:10:26 +0200

    Updates

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,128 @@
 <script>
-	export let segment;
+  export let segment;
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
+  @import url("https://fonts.googleapis.com/css?family=Ubuntu&display=swap");
+  nav {
+    border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+    font-weight: 300;
+    padding: 0 1em;
+    display: grid;
+    grid-template-columns: repeat(2, 50%);
+    align-items: center;
+  }
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
+  ul {
+    margin: 0;
+    padding: 0;
+  }
 
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+  /* clearfix */
+  ul::after {
+    content: "";
+    display: block;
+    clear: both;
+  }
 
-	/* li {
-		display: block;
-		float: left;
-	} */
+  li {
+    display: block;
+    float: right;
+    text-align: right;
+  }
 
-	/* .selected {
-		position: relative;
-		display: inline-block;
-	} */
+  .selected {
+    position: relative;
+    display: inline-block;
+  }
 
-	/* .selected::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	} */
+  .selected::after {
+    position: absolute;
+    content: "";
+    width: calc(100% - 1em);
+    height: 2px;
+    background-color: rgb(0, 0, 0);
+    display: block;
+    bottom: -1px;
+  }
 
-	/* a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	} */
+  a {
+    text-decoration: none;
+    padding: 1em 0.5em;
+    display: block;
+  }
+  .menu-items {
+    text-align: right;
+  }
 </style>
 
 <nav>
-	<ul>
-		<!-- <li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
-		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li> -->
+  <div>
+   <a class={segment === undefined} href=".">
+   <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      style="isolation:isolate"
+      viewBox="185.375 149.841 76.625 15.694"
+      width="76.625pt"
+      height="15.694pt">
+      <defs>
+        <clipPath id="_clipPath_B7dVjRj8HdGs7yxqpLM8qdAvx6XYJJgV">
+          <rect x="185.375" y="149.841" width="76.625" height="15.694" />
+        </clipPath>
+      </defs>
+      <g clip-path="url(#_clipPath_B7dVjRj8HdGs7yxqpLM8qdAvx6XYJJgV)">
+        <text
+          transform="matrix(1,0,0,1,200,162.889)"
+          style="font-family:'Ubuntu';font-weight:400;font-size:14px;font-style:normal;fill:#4f4f4f;stroke:none;">
+          learnweb
+        </text>
+        <g>
+          <g>
+            <g>
+              <g>
+                <path
+                  d=" M 190.688 152.375 C 193.663 152.375 196 154.712 196
+                  157.688 C 196 160.663 193.663 163 190.688 163 C 187.712 163
+                  185.375 160.663 185.375 157.688 C 185.375 154.712 187.712
+                  152.375 190.688 152.375 Z M 190.688 153.969 C 188.671 153.969
+                  186.969 155.671 186.969 157.688 C 186.969 159.704 188.671
+                  161.406 190.688 161.406 C 192.704 161.406 194.406 159.704
+                  194.406 157.688 C 194.406 157.265 194.315 156.848 194.207
+                  156.425 C 193.888 156.849 193.344 157.156 192.813 157.156 C
+                  191.962 157.156 191.219 156.413 191.219 155.563 C 191.219
+                  155.031 191.527 154.487 191.95 154.167 C 191.526 154.06 191.11
+                  153.969 190.688 153.969 Z "
+                  fill="rgb(74,134,0)" />
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+   
+   </a>
+    
+  </div>
+  <div class="menu-items">
+    <ul>
+      <li>
+        <a class={segment === undefined ? 'selected' : ''} href=".">home</a>
+      </li>
+      <li>
+        <a class={segment === 'about' ? 'selected' : ''} href="about">about</a>
+      </li>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+      <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<!-- <li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li> -->
-	</ul>
+      <li>
+        <a
+          rel="prefetch"
+          class={segment === 'blog' ? 'selected' : ''}
+          href="blog">
+          blog
+        </a>
+      </li>
+    </ul>
+  </div>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,15 @@
 <style>
-  @import url("https://fonts.googleapis.com/css?family=Overpass|Ruluko&display=swap");
+  @import url("https://fonts.googleapis.com/css?family=Ubuntu&display=swap");
 
   #logo {
     margin-left: 10px;
     margin-top: 40px;
   }
 
-  h3 {
-    margin-left: 12px;
+  h2 {
     font-family: "Overpass", sans-serif;
-    font-size: 1.2rem;
-    font-weight: 300;
+    font-size: 1.5rem;
+    font-weight: 500;
   }
 
   #small {
@@ -20,7 +19,7 @@
   }
 
   .startImage-small {
-    display: block
+    display: block;
   }
 
   .startImage-big {
@@ -29,21 +28,67 @@
 
   @media only screen and (min-width: 415px) {
     .page-container {
+      border: 0.6rem solid white;
+      border-bottom: 0;
       background: url("");
       background-size: cover;
+      background: #fafafa;
+      padding: 0 4rem;
+      border-radius: 8px;
     }
     .startImage-small {
       display: none;
       z-index: -1;
     }
     .startImage-big {
-    display: block;
-  }
+      display: block;
+    }
     img {
-    width: 100%;
-    /* max-width: 400px; */
-    margin: 0 0 1em 0;
+      width: 100%;
+      /* max-width: 400px; */
+      margin: 0 0 1em 0;
+    }
+    #logo {
+      margin-left: 0;
+      margin-top: 40px;
+    }
   }
+  .service {
+    background: white;
+    display: flex;
+    font-size: 0.8rem;
+  }
+
+  .frameworks {
+    width: 50%;
+    border: 0.6rem solid white;
+    padding: 1.8em;
+    padding: 1.8em;
+    border-radius: 1rem;
+    background: rgb(25, 164, 161);
+    background: radial-gradient(
+      circle,
+      rgba(25, 164, 161, 1) 0%,
+      rgba(25, 163, 160, 1) 57%,
+      rgba(20, 147, 144, 1) 91%
+    );
+    color: #f8f8f8;
+  }
+
+  .lms {
+    width: 50%;
+    border: 0.6rem solid white;
+    padding: 1.8em;
+    padding-bottom: 1em;
+    border-radius: 1rem;
+    background: rgb(59, 162, 231);
+    background: radial-gradient(
+      circle,
+      rgba(59, 162, 231, 1) 0%,
+      rgba(56, 164, 235, 1) 47%,
+      rgba(60, 157, 221, 1) 98%
+    );
+    color: #f8f8f8;
   }
 </style>
 
@@ -57,32 +102,53 @@
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     style="isolation:isolate"
-    viewBox="230.5 136.75 183 55.47"
-    width="169pt"
-    height="55.47pt">
+    viewBox="0 0 205 53.838"
+    width="205pt"
+    height="53.838pt">
     <defs>
-      <clipPath id="_clipPath_qAVaLgp9g35h5DwJHxwUtjy0breOmNEL">
-        <rect x="230.5" y="136.75" width="183" height="55.47" />
+      <clipPath id="_clipPath_0MDbOLkMMELSOJaGz2vTt8LnUrsXbtJY">
+        <rect width="205" height="53.838" />
       </clipPath>
     </defs>
-    <g clip-path="url(#_clipPath_qAVaLgp9g35h5DwJHxwUtjy0breOmNEL)">
+    <g clip-path="url(#_clipPath_0MDbOLkMMELSOJaGz2vTt8LnUrsXbtJY)">
       <text
-        transform="matrix(1,0,0,1,230.5,180.622)"
-        style="font-family:'Ruluko';font-weight:400;font-size:48px;font-style:normal;fill:#4a4a55;stroke:none;">
+        transform="matrix(1,0,0,1,0,44.736)"
+        style="font-family:'Ubuntu';font-weight:400;font-size:48px;font-style:normal;fill:#4f4f4f;stroke:none;">
         learnweb
       </text>
       <text
-        transform="matrix(1,0,0,1,230.5,180.652)"
-        style="font-family:'Ruluko';font-weight:400;font-size:48px;font-style:normal;fill:#4a4a55;stroke:none;">
+        transform="matrix(1,0,0,1,0,44.766)"
+        style="font-family:'Ubuntu';font-weight:400;font-size:48px;font-style:normal;fill:#4f4f4f;stroke:none;">
         learnweb
       </text>
     </g>
   </svg>
-  <h3>Uveckling av digitala lärresurser</h3>
+  <h2>Uveckling av digitala lärresurser</h2>
   <div class="startImage-small">
-    <img id="small" style="padding-top:3rem;" src="larresurs.png" alt="Lärresurs" />
+    <img
+      id="small"
+      style="padding-top:3rem;"
+      src="larresurs.png"
+      alt="Lärresurs" />
   </div>
   <div class="startImage-big">
-    <img width="800px" style="padding-top:3rem;" src="larresurs_big.jpg" alt="Lärresurs" />
+    <img width="800px" src="larresurs_big.png" alt="Lärresurs" />
+  </div>
+</div>
+<div class="service">
+  <div class="frameworks">
+    <h2>Webbdesign</h2>
+    <p>
+      Vår metod gör vi det möjligt för företag och utbildningsorganisationer
+      att arbeta effektivt med kunskapshantering.
+    </p>
+    <button style="float:right;">Läs mer</button>
+  </div>
+  <div class="lms">
+    <h2>System</h2>
+    <p>
+      Vi är experter på LMS och hur digitala lösningar kan skapa mervärde för både utbildare och studenter.
+    </p>
+    <button style="float: right;">Läs mer</button>
   </div>
 </div>
