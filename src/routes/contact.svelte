567bb702a0d1952baec6f4c9c5af92a3903792a0
Leif Johansson <leif.lernia@gmail.com>
2019-10-08 21:51:07 +0200

    contact mobile

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -20,69 +20,72 @@
 
 <style>
   @import url("https://fonts.googleapis.com/css?family=Quicksand&display=swap");
-  #form-header {
-    font-family: "Quicksand", sans-serif;
-  }
+  @media (min-width: 40rem) {
+    #form-header {
+      font-family: "Quicksand", sans-serif;
+    }
 
-  h1 {
-    font-size: 2rem;
-  }
-  #form-header > p {
-    color: #69778b;
-  }
-  form {
-    -webkit-font-smoothing: antialiased;
-    padding: 10px 18px 20px 24px;
-  }
-  .contact-form {
-    box-sizing: border-box;
-    border-radius: 4px;
-    box-shadow: 0 50px 100px -20px rgba(50, 50, 93, 0.25),
-      0 30px 60px -30px rgba(0, 0, 0, 0.3),
-      0 -18px 60px -10px rgba(0, 0, 0, 0.025);
-    max-width: 545px;
-    background: #fff;
-  }
-  #form-header {
-    display: block;
-    margin-top: 0.8rem;
-    margin-bottom: 3rem;
-    width: 100%;
-  }
-  .form-row {
-    display: flex;
-  }
-  .form-row > label {
-    -webkit-font-smoothing: antialiased;
-    text-rendering: optimizeLegibility;
-    box-sizing: border-box;
-    -webkit-tap-highlight-color: transparent;
-    display: block;
-    color: #424770;
-    font-size: 15px;
-    font-weight: 500;
-    letter-spacing: normal;
-    line-height: 26px;
-    margin-right: 20px;
-    align-self: center;
-    flex: 32%;
-  }
-  .clearfix::after {
-    content: "";
-    clear: both;
-    display: table;
-  }
-  input, textarea {
-    display: block;
-    border: none;
-    outline: none;
-    border-radius: 4px;
-    color: #32325d;
-    font-weight: 400;
-    font-size: 15px;
-    background-color: #f6f9fc;
-    padding: 5px 20px 8px 13px;
-    flex: 68%;
+    h1 {
+      font-size: 2rem;
+    }
+    #form-header > p {
+      color: #69778b;
+    }
+    form {
+      -webkit-font-smoothing: antialiased;
+      padding: 10px 18px 20px 24px;
+    }
+    .contact-form {
+      box-sizing: border-box;
+      border-radius: 4px;
+      box-shadow: 0 50px 100px -20px rgba(50, 50, 93, 0.25),
+        0 30px 60px -30px rgba(0, 0, 0, 0.3),
+        0 -18px 60px -10px rgba(0, 0, 0, 0.025);
+      max-width: 545px;
+      background: #fff;
+    }
+    #form-header {
+      display: block;
+      margin-top: 0.8rem;
+      margin-bottom: 3rem;
+      width: 100%;
+    }
+    .form-row {
+      display: flex;
+    }
+    .form-row > label {
+      -webkit-font-smoothing: antialiased;
+      text-rendering: optimizeLegibility;
+      box-sizing: border-box;
+      -webkit-tap-highlight-color: transparent;
+      display: block;
+      color: #424770;
+      font-size: 15px;
+      font-weight: 500;
+      letter-spacing: normal;
+      line-height: 26px;
+      margin-right: 20px;
+      align-self: center;
+      flex: 32%;
+    }
+    .clearfix::after {
+      content: "";
+      clear: both;
+      display: table;
+    }
+    input,
+    textarea {
+      display: block;
+      border: none;
+      outline: none;
+      border-radius: 4px;
+      color: #32325d;
+      font-weight: 400;
+      font-size: 15px;
+      background-color: #f6f9fc;
+      padding: 5px 20px 8px 13px;
+      flex: 68%;
+    }
   }
 </style>
 
@@ -92,10 +95,7 @@
 
 <div id="form-header">
   <h1>Kontakta oss</h1>
-  <p>
-    Vi skulle bli jätteglada om du tog kontakt med oss. Fyll i dina uppgifter så
-    hör vi av oss så fort vi kan.
-  </p>
+  <p>Skriv till oss så hör vi av oss så fort vi kan.</p>
 </div>
 <div class="contact-form">
   <form
@@ -105,17 +105,20 @@
 
     <div class="form-row">
       <label class="first-name">Namn</label>
-      <input type="text" name="name" placeholder="Kalle Karlsson" />
+      <input type="text" name="name" />
     </div>
     <div class="form-row">
       <label class="email">E-post</label>
-      <input type="email" name="email" placeholder="kalle@exempel.se" />
+      <input type="email" name="email" placeholder="@" />
     </div>
     <div class="form-row">
       <label style="align-self: flex-start; padding-top: 12px;">
         Meddelande
       </label>
-      <textarea name="comment" maxlength="500" form="contact-form" placeholder="Berätta vad vi kan göra för dig" />
+      <textarea
+        name="comment"
+        maxlength="500"
+        form="contact-form" />
     </div>
     <div style="float: right;">
       <button id="send" type="submit">Skicka</button>
